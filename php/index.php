7caf9a447b54b4553f1b2b940be201d7615cde74
Marly <r.marlyp@gmail.com>
2024-09-28 11:42:01 -0500

    Creación de función pop

--- php/index.php
+++ php/index.php
@@ -29,17 +29,28 @@ class Stack
 {
     public $head;
     public $tail;
-
+    
     public function __construct()
     {
         $this->head = null;
         $this->tail = null;
     }
-
+    
     public function push($value)
     {
         $nodo = new Nodo($value);
-        $this->head = $nodo;
+
+        if ($this->isEmpty()) {
+            $this->head = $nodo;
+            $this->tail = $nodo;    
+        } else {
+            $nodo->setNext($this->head);
+            $this->head = $nodo;
+        }
+    }
+
+    public function pop() {
+       $this->head = $this->head->getNext();
     }
 
     //? Debo se;alar directamente el valor del nodo,
@@ -52,8 +63,15 @@ class Stack
             return;
         }
 
-        echo $this->head->value . "<br>";
-        echo $this->head->getNext() . "<br>";
+        $current = $this->head;
+
+        while ($current != null) {
+            echo $current->value . "<br>";
+            $current = $current->getNext();
+        }
+
+
+        
     }
 
     public function isEmpty()
@@ -64,7 +82,20 @@ class Stack
 
 $stack = new Stack();
 $stack->push("First node of information");
+$stack->push("Second node of information");
+$stack->push("Third node of information");
+$stack->push("Fourth node of information");
+$stack->push("Fifth node of information");
+
 $stack->print();
+echo "<br>---------------------- <br>";
+$stack->pop();
+$stack->print();
+
+// Estoy creando un stack con nodos, el stack tiene un head y un tail, el head es el primer nodo que se inserta y el tail es el último nodo que se inserta. El stack tiene un método push que inserta un nodo al principio del stack. El stack tiene un método print que imprime todos los nodos del stack. El stack tiene un método isEmpty que devuelve true si el stack está vacío y false si no lo está.
+
+// Primero me va a imprimir 'creando nodo que es el momento en que se está creando nodo' y luego imprimo el valor de cada nodo
+
 
 // $nodo = new Nodo("Hola");
 
